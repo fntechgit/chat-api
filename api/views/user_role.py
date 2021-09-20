374f078040bb9bb9d0f03e79a3ecebedfbb1d499
smarcet <smarcet@gmail.com>
2021-09-20 19:57:49 -0300

    Added log info
    Signed-off-by: smarcet <smarcet@gmail.com>

--- api/views/user_role.py
+++ api/views/user_role.py
@@ -109,7 +109,8 @@ class UserRolesCreateAPIView(ViewSet):
             res = service.sso\
                 (
                     token_info['user_id'],
-                    '{} {}'.format(token_info['user_first_name'],token_info['user_last_name']),
+                    token_info['user_first_name'],
+                    token_info['user_last_name'],
                     role,
                     token_info['user_pic'],
                 )
